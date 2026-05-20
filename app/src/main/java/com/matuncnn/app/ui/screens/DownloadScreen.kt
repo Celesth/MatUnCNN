9c20cc5e4bad20da822a0ca7f3e7b214decec2e8
Celesth <krnlkuro@gmail.com>
2026-05-20 21:38:28 +0530

    dwl screen, repo info screen theme fix

--- app/src/main/java/com/matuncnn/app/ui/screens/DownloadScreen.kt
+++ app/src/main/java/com/matuncnn/app/ui/screens/DownloadScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import com.matuncnn.app.ui.theme.AppColors
 import androidx.compose.runtime.Composable
--- app/src/main/java/com/matuncnn/app/ui/screens/RepoInfoScreen.kt
+++ app/src/main/java/com/matuncnn/app/ui/screens/RepoInfoScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import com.matuncnn.app.ui.theme.AppColors
 import androidx.compose.runtime.Composable
