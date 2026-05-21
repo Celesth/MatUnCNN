48c7b36537ef24039940a0e9a6e5ca1d247c64ed
Celesth <krnlkuro@gmail.com>
2026-05-21 21:00:16 +0530

    cyan

--- app/src/main/java/com/matuncnn/app/ui/theme/AppColors.kt
+++ app/src/main/java/com/matuncnn/app/ui/theme/AppColors.kt
@@ -3,14 +3,14 @@ package com.matuncnn.app.ui.theme
 import androidx.compose.ui.graphics.Color
 
 object AppColors {
-    val Navy = Color(0xFF0D1321)
-    val Dark = Color(0xFF1D2D44)
-    val Mid = Color(0xFF3E5C76)
-    val Slate = Color(0xFF748CAB)
+    val Navy = Color(0xFF0d0d0d)
+    val Dark = Color(0xFF141414)
+    val Mid = Color(0xFF5bc0be)
+    val Slate = Color(0xFF6fffe9)
     val Cream = Color(0xFFF0EBD8)
 
-    val statusProcessing = Color(0xFF1976D2)
-    val statusSuccess = Color(0xFF388E3C)
-    val statusError = Color(0xFFD32F2F)
-    val statusIdle = Color(0xFF757575)
+    val statusProcessing = Color(0xFF38E1FF)
+    val statusSuccess = Color(0xFF5EFF97)
+    val statusError = Color(0xFF9F0000)
+    val statusIdle = Color(0xFF898989)
 }
